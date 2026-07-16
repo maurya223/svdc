717592b715dc4762f18c531f834c38fb68e596ee
Monu Maurya <youremail@example.com>
2026-07-17 00:00:37 +0530

    everything_is_fine

--- clinic/settings.py
+++ clinic/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1'
+    default='localhost,127.0.0.1,svdc-9ga4.onrender.com'
 ).split(',')
 
 # Security settings for production
